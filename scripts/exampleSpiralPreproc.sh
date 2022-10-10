5fceaae01a9d690d202fa79c952d0dbd151c6fd0
Luke Edwards <lukejoele+github@gmail.com>
2022-10-10 11:39:49 +0200

    Add command line options to use slspec file

--- scripts/exampleSpiralPreproc.sh
+++ scripts/exampleSpiralPreproc.sh
@@ -18,7 +18,8 @@ mrconvert $DWI_in -fslgrad $bvec_in $bval_in $outdir/DWI_in.mif
 # let mrtrix take care of providing eddy input and output
 # readout_time may need to be longer to satisfy eddy's sanity checking (but not so long that it starts expecting large shifts)
 # first level model (flm) is set to linear to avoid overfitting as spiral data should already have been corrected for eddy currents
-dwifslpreproc $outdir/DWI_in.mif $outdir/DWI_out.mif -rpe_none -pe_dir ap -readout_time 0.001 -eddy_slspec $slspec -eddyqc_all $outdir -eddy_options " --flm=linear --repol --data_is_shelled "
+# mporder is recommended to be somewhere between N/4 and N/2, where N is the number of excitations
+dwifslpreproc $outdir/DWI_in.mif $outdir/DWI_out.mif -rpe_none -pe_dir ap -readout_time 0.001 -eddy_slspec $slspec -eddyqc_all $outdir -eddy_options " --flm=linear --repol --data_is_shelled --mporder=13 --ol_type=both "
 
 # convert mrtrix output to nii and bvec/bval
 mrconvert $outdir/DWI_out.mif -export_grad_fsl $outdir/DWI_out.bvec $outdir/DWI_out.bval $outdir/DWI_out.nii.gz
