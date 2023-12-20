2051cbd71612ce20ab96417c15b70dbc3f4c30c5
mavel101 <mavel101@hhu.de>
2023-12-20 18:43:11 +0100

    add keep flag in spiral processing

--- scripts/process_spiral.sh
+++ scripts/process_spiral.sh
@@ -96,8 +96,8 @@ fslmaths "${IN_FILE_PREFIX}_moco_unwarped.nii.gz" -mul "${IN_FILE_PREFIX}_moco_u
 # Spherical harmonic decomposition
 if $mle_flag; then
     matlab -nodisplay -r "addpath ${SCRIPTPATH}/../AxonRadiusMapping/;fitSH('${IN_FILE_PREFIX}_moco_unwarped_bet.nii.gz', '${IN_FILE_PREFIX}_moco_unwarped_meanb0_bet_mask.nii.gz', '${IN_FILE_PREFIX}_noise_map.nii.gz', '${IN_FILE_PREFIX}_moco_unwarped_bet.bval', '${IN_FILE_PREFIX}_moco_unwarped_bet.bvec', '${IN_FILE_PREFIX}');exit"
-    gzip -f "${IN_FILE_PREFIX}_sh_b6000_powderavg.nii"
-    gzip -f "${IN_FILE_PREFIX}_sh_b30000_powderavg.nii"
+    gzip -k -f "${IN_FILE_PREFIX}_sh_b6000_powderavg.nii"
+    gzip -k -f "${IN_FILE_PREFIX}_sh_b30000_powderavg.nii"
 else
     if $mag_flag; then
         amp2sh -force -lmax 6 -shells 0,6000 -normalise -rician "${IN_FILE_PREFIX}_noise_map.nii.gz" -fslgrad "${IN_FILE_PREFIX}_moco_unwarped_bet.bvec" "${IN_FILE_PREFIX}_moco_unwarped_bet.bval" "${IN_FILE_PREFIX}_moco_unwarped_bet.nii.gz" "${IN_FILE_PREFIX}_sh_b6000.nii.gz"
